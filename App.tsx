8289dbceff90c0202aa8a5eb779cbe390918bdb5
larissalvesf2@gmail.com <larissalvesf2@gmail.com>
2022-07-14 20:28:22 -0300

    feat: add categories

--- App.tsx
+++ App.tsx
@@ -1,41 +1,38 @@
+import React from "react";
 
-import React from 'react';
-
-import { ThemeProvider } from 'styled-components';
+import { ThemeProvider } from "styled-components";
 
 import {
   useFonts,
   Poppins_400Regular,
   Poppins_500Medium,
-  Poppins_700Bold
-} from '@expo-google-fonts/poppins'
-import * as  SplashScreen from 'expo-splash-screen'
+  Poppins_700Bold,
+} from "@expo-google-fonts/poppins";
+import * as SplashScreen from "expo-splash-screen";
 
-import theme from './src/global/styles/theme';
+import theme from "./src/global/styles/theme";
 
-import { Dashboard } from './src/screens/Dashboard';
-
-
+import { Dashboard } from "./src/screens/Dashboard";
+import { Register } from "./src/screens/Register";
+import { CategorySelect } from "./src/screens/CategorySelect";
 
 export default function App() {
   SplashScreen.preventAutoHideAsync();
-  const[fontsLoaded] = useFonts({
+  const [fontsLoaded] = useFonts({
     Poppins_400Regular,
     Poppins_500Medium,
-    Poppins_700Bold
+    Poppins_700Bold,
   });
-  if(!fontsLoaded) {
+  if (!fontsLoaded) {
     return null;
   }
   SplashScreen.hideAsync();
 
   return (
     <ThemeProvider theme={theme}>
-      <Dashboard/>
+      {/* <Dashboard/> */}
+      <Register />
+      {/* <CategorySelect /> */}
     </ThemeProvider>
-  )
-  
-  
+  );
 }
-
-
--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,33 +1,120 @@
-import React from 'react';
-import {Feather} from '@expo/vector-icons';
+import React from "react";
 
-import { Container, 
-  Header, 
+import { HighlightCard } from "../../Components/HighlightCard";
+import {
+  TransactionCard,
+  TransactionCardProps,
+} from "../../Components/TransactionCard";
+
+import {
+  Container,
+  Header,
   UserWrapper,
   UserInfo,
   Photo,
   User,
   UserGretting,
   UserName,
-} from './styles';
+  Icon,
+  HighlightCards,
+  Transactions,
+  Title,
+  TransactionList,
+} from "./styles";
 
-export function Dashboard(){
+export interface DataListProps extends TransactionCardProps {
+  id: string;
+}
+
+export function Dashboard() {
+  const data: DataListProps[] = [
+    {
+      id: "1",
+      type: "positive",
+      title: "Desenvolvimento de site",
+      amount: "R$ 12.000,00",
+      category: {
+        name: "Vendas",
+        icon: "dollar-sign",
+      },
+      date: "13/04/2022",
+    },
+    {
+      id: "2",
+      type: "negative",
+      title: "Hamburguer Pizzy",
+      amount: "R$ 55,00",
+      category: {
+        name: "Alimentação",
+        icon: "coffee",
+      },
+      date: "13/04/2022",
+    },
+    {
+      id: "3",
+      type: "negative",
+      title: "Aluguel do Apartamento",
+      amount: "R$ 1.200,00",
+      category: {
+        name: "Casa",
+        icon: "shopping-bag",
+      },
+      date: "27/04/2022",
+    },
+  ];
+
   return (
     <Container>
       <Header>
         <UserWrapper>
-        <UserInfo>
-          <Photo source={{uri: 'https://avatars.githubusercontent.com/u/82070562?v=4'}}/>
-          <User>
-            <UserGretting>Olá, </UserGretting>
-            <UserName>Larissa</UserName>
-          </User>
-        </UserInfo>
-        <Feather name="power" />
-        </UserWrapper>
-        
-      </Header>
-    </Container>
-  )
-}
+          <UserInfo>
+            <Photo
+              source={{
+                uri: "https://avatars.githubusercontent.com/u/82070562?v=4",
+              }}
+            />
+            <User>
+              <UserGretting>Olá, </UserGretting>
+              <UserName>Larissa</UserName>
+            </User>
+          </UserInfo>
 
+          <Icon name="power" />
+        </UserWrapper>
+      </Header>
+
+      <HighlightCards>
+        <HighlightCard
+          type="up"
+          title="Entrada"
+          amount="R$ 17.400,00"
+          lastTransaction="Última entrada dia 13 de abril"
+        />
+
+        <HighlightCard
+          type="down"
+          title="Saídas"
+          amount="R$ 1.259,00"
+          lastTransaction="Última saída dia 03 de abril"
+        />
+        <HighlightCard
+          type="total"
+          title="Total"
+          amount="R$ 16.141,00"
+          lastTransaction="01 à 16 de abril"
+        />
+      </HighlightCards>
+
+      <Transactions>
+        <Title>Listagem</Title>
+        <TransactionList
+          data={data}
+          keyExtractor={(item) => (item as DataListProps).id}
+          renderItem={({ item }) => (
+            <TransactionCard data={item as DataListProps} />
+          )}
+        />
+      </Transactions>
+    </Container>
+  );
+}
